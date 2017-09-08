1599a22a3d93162ed672085ab7603bc549e0e393
noelpei0461 <31324311+noelpei0461@users.noreply.github.com>
2017-09-08 10:00:46 +0800

    Update Inclass4.m

--- Inclass4.m
+++ Inclass4.m
@@ -5,7 +5,39 @@
 %2" on its own line, then 9 lines with 7 random numbers each. (b) Write code
 %that reads only the first line of random numbers in the file and stores
 %them as numbers in an array. 
+answer a:
+
+xx=rand(10,13);
+yy=rand(9,7);
+
+filename='random.txt';
+fid=fopen(filename,'w');
+fprintf(fid,'Random numbers 1\n');
+dlmwrite('random.txt',xx,'-append');
+fid=fopen(filename,'a')
+fprintf(fid,'Random numbers 2\n');
+dlmwrite('random.txt',yy,'-append');
+
+answer b:
+fid=fopen(filename)
+for i=1:2
+    tline=fgetl(fid)
+    if ~ischar(tline),
+        break;
+    end
+    disp(tline)
+end
+
+tarray=str2num(tline)
+tarray
 
 % 2. Write a function that takes an array as input and returns a logical
 % variable which is true if the sum of the numbers in the array is greater
 % than or equal to 10 and false if it is less than 10. 
+
+ss=sum(tnum)
+if ss>=10
+    ss>=10
+else
+    ss>=10
+end
